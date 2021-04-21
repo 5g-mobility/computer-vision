10f06081e9ec0b32fb2369a1404b5e2222ab3b05
hugofpaiva <hugofpaiva@ua.pt>
2021-04-21 02:29:45 +0100

    Celery demonstration confirmed

--- tasks.py
+++ tasks.py
@@ -5,4 +5,4 @@ mock_data = [{"car":4, "person":2, "bike":5}, {"car":9, "person":1, "bike":0}]
 
 app = Celery('tasks', broker='amqp://django:djangopass@localhost:5672/celery')
 
-app.send_task('mobility_5g_rest_api.tasks.print_json', kwargs={'json':mock_data})
+app.send_task('mobility_5g_rest_api.tasks.sensor_fusion', kwargs={'json':mock_data})
